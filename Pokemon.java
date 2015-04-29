b91876357c7d21b100575192890647c125b4effd
RyanIDavis <ryanidavis@yahoo.com>
2015-04-29 12:07:44 -0400

    Fourth Try

--- Pokemon.java
+++ Pokemon.java
@@ -12,35 +12,36 @@ public class Pokemon {
 	private int attack;
     private Move[] moves;
 
-    // If Eevee
-        // HP start 55, max 140
-        // A start 55, max 85
-        // D start 50, max 80
+    switch (pokemonName){
+        case Eevee:
+            health = 55; //max 140
+            attack = 55; //max 85
+            defense = 50; //max 80
 
-    // If Pikachu
-        // HP start 35, max 120
-        // A start 55, max 85
-        // D start 30, max 60
+        case Pikachu:
+            health = 35, max 120
+            attack = 55, max 85
+            defense = 30, max 60
 
-    // If Charmander
-        // HP start 40, max 125
-        // A start 50, max 85
-        // D start 45, max 75
+        case Charmander:
+            health = 40, max 125
+            attack = 50, max 85
+            defense = 45, max 75
 
-    // If Meowth
-        // HP start 40, max 125
-        // A start 45, max 75
-        // D start 35, max 65
+        case Meowth:
+            health = 40, max 125
+            attack = 45, max 75
+            defense = 35, max 65
 
-    // If Houndour
-        // HP start 45, max 130
-        // A start 60, max 90
-        // D start 30, max 60
+        case Houndour:
+            health = 45, max 130
+            attack = 60, max 90
+            defense = 30, max 60
 
-    // If Koffing
-        // HP start 40, max 125
-        // A start 65, max 95
-        // D start 95, max 125
+        case Koffing:
+            health = 40, max 125
+            attack = 65, max 95
+            defense = 95, max 125
 
 
     public Pokemon(String name; int lValue; int hValue; int dValue; int aValue){
