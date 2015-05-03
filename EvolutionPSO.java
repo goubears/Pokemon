11e3765269a22b30320e80d9f65e3e2c3fbe0af0
Aams1000 <amillers@bowdoin.edu>
2015-05-03 18:14:51 -0400

    Fixed Move class. One remaining error with moves array initialization.

---
+++ EvolutionPSO.java
@@ -0,0 +1,119 @@
+		/****************************************
+         *                                      *
+         *          EvolutionPSO class          *
+         *         Andrew Miller-Smith          *
+         *                                      *
+         ****************************************/
+
+        /*
+
+        Description:    This algorithm simulates Ant Colony Optimization on the traveling salesman problem. Paths are constructed between cities, and ants
+                        walk these paths to complete tours. After each tour is complete, paths receive pheromone updates. Updates are proportional to the
+                        frequency which which the ants use the path. Paths on the shortest tour receive extra boosts in order to steer the ants toward
+                        the best current solution. Pheromone evaporates over time.
+
+                        The Enrichment Center promises to always provide a safe testing environment. In dangerous testing environments, 
+                        the Enrichment Center promises to always provide useful advice. For instance, the floor here will kill you. Try to avoid it.
+        */
+
+import java.util.*;
+import java.io.*;
+import java.util.Random;
+
+public class EvolutionPSO {
+	
+    //Vectors and arrays for storing tours the ants make and paths between cities
+    private static Vector<Pokemon> pokemon;
+    private static Vector<Path> pathsVector;
+    private static Path[][] paths;
+    private static Vector<City> cities;
+    
+    //private static int NUM_ITERATIONS;
+
+    //variables for storing best solution so far
+    private static Tour bestSolution;
+    private static double bestLength = Double.POSITIVE_INFINITY;
+
+    //time limit to stop algorithm. set at eight minutes (ms)
+    private static final long TIME_LIMIT = 480000;
+
+    //random number generators
+    private static Random rand = new Random();
+
+    //named constants assigned upon receiving parameters
+    private static int NUM_ITERATIONS = 50;
+    private static int totalIterations;
+    private static long duration;
+
+    public static void evolutionPSO(Vector<Pokemon> breedingPopulation, int iterations)
+    {
+        //set up variables
+        pokemon = breedingPopulation;
+        NUM_ITERATIONS = iterations;
+        
+        //start timer
+        long startTime = System.currentTimeMillis();
+        long endTime = 0;
+        duration = 0;
+
+            //update time
+            endTime = System.currentTimeMillis();
+            duration = (endTime - startTime)/1000;
+
+        
+
+        //print out our findings
+        // System.out.println("\n******************* Elitist Ant Algorithm Results *******************");
+        // System.out.println("Please note that we have added a consequence for failure. Any contact with the chamber floor will result in an 'unsatisfactory' mark on your official testing record, followed by death. Good luck!");
+        //we'll need to print out the name of the file we are reading. This should come from the user-input parsing part...maybe make it a global variable?
+        // System.out.println("Name of file: " + FILE_NAME);
+
+        //same for NUM_VARIABLES and number of clauses
+        // System.out.println("Number of cities: " + NUM_CITIES);
+        // System.out.println("Number of ants: " + NUM_ANTS);
+        
+        // if (bestLength < TARGET_OPTIMUM)
+        // {
+        //     System.out.println("Target optimum reached early.");   
+        // }    
+        
+        // if (duration > TIME_LIMIT)
+        // {
+        //     System.out.println("Time limit reached.");
+        // }
+       
+        // System.out.println("Number of iterations: " + totalIterations);
+        
+        //we know how many clauses we have satisfied
+        // System.out.println("Length of shortest path: " + bestLength);
+        
+        // System.out.println("\nThis method took: " + duration + " milliseconds.");
+
+    }
+
+    public static int getTotalIterations(){
+        return totalIterations;
+    }
+
+    public static double getBestLength(){
+        return bestLength;
+    }
+
+    public static long getDuration(){
+        return duration;
+    }
+
+    //method to fill temp cities vector. DON'T GET RID OF ME! I AM A TIMELSS RELIC OF THE PAST
+    public static void generatePokemon()
+    {
+        for (int i = 0; i < NUM_CITIES; i++)
+        {
+            City newCity = new City(rand.nextInt(100), rand.nextInt(100), i);
+            cities.add(newCity);
+
+            //A
+            newCity = null;
+        }
+    }
+
+}
--- Move.java
+++ Move.java
@@ -2,68 +2,92 @@
 import java.io.*;
 import java.util.*;
 
-	public class Move {
-		
-		//Variables
-		private String pokemonName;
-		private int attack;
-		private int accuracy;
+public class Move {
+	
+	//Variables
+	private String pokemonName;
+	private int attack;
+	private int accuracy;
 
-		//Move list
-		private String[] possibleMoves = new String[6];
-		private int[] attackPower = new int[6];
-		private int[] attackHitChance = new int[6];
-		private String[] moves = new String[3];
-		private Random random = new Random();
+	//Move list
+	private String[] possibleMoves;
+	private int[] attackPower;
+	private int[] attackHitChance;
+	private String[] moves = new String[3];
+	private Random random = new Random();
 
 
-		public Move(String name)
+	public Move(String name)
+	{
+		for (int i=0; i<2; i++)
 		{
-			for (int i=0; i<2; i++)
-			{
-				moves[i] = random.nextInt(3);
-			}		        
-		}
-
-		public allMoves(String name; int power; int hitChance){
-			pokemonName = name;
-			attack = power;
-		    accuracy = hitChance;
-
-	        if (pokemonName.equals("Eevee")){
-	            possibleMoves = {"Quick Attack", "Bite", "Take Down", "Move4", "Move5", "Move6"}; 
-	            attackPower = {40, 60, 100};
-	            attackHitChance = {100, 100, 75};
-	        }
-
-	        if (pokemonName.equals("Pikachu")){
-	            possibleMoves = {"Quick Attack", "Thunder Shock", "Slam", "Move4", "Move5", "Move6"}; 
-	            attackPower = {30, 50, 80};
-	            attackHitChance = {100, 100, 85};
-	        }
-
-	        if (pokemonName.equals("Charmander")){
-	            possibleMoves = {"Ember", "Slash", "Flamethrower", "Move4", "Move5", "Move6"}; 
-	            attackPower = {40, 70, 90};
-	            attackHitChance = {100, 85, 75};
-	        }
-
-	        if (pokemonName.equals("Meowth")){
-	            possibleMoves = {"Scratch", "Bite", "Slash", "Move4", "Move5", "Move6"}; 
-	            attackPower = {40, 60, 70};
-	            attackHitChance = {100, 90, 85};
-	        }
-
-	        if (pokemonName.equals("Houndour")){
-	            possibleMoves = {"Bite", "Feint Attack", "Crunch", "Move4", "Move5", "Move6"}; 
-	            attackPower = {60, 70, 80};
-	            attackHitChance = {100, 95, 90};
-	        }
-
-	        if (pokemonName.equals("Koffing")){
-	            possibleMoves = {"Smog", "Tackle", "Sludge", "Move4", "Move5", "Move6"}; 
-	            attackPower = {30, 50, 70};
-	            attackHitChance = {100, 90, 80};
-	        }
-	    }
+			moves[i] = (random.nextInt(3));
+		}		        
 	}
+
+	public void allMoves(String name, int power, int hitChance){
+		pokemonName = name;
+		attack = power;
+	    accuracy = hitChance;
+
+        if (pokemonName.equals("Eevee")){
+            String[] pokemonMoves = {"Quick Attack", "Bite", "Take Down", "Move4", "Move5", "Move6"};
+            possibleMoves = pokemonMoves;
+            int[] powers = {40, 60, 100};
+            attackPower = powers;
+            int[] accuracies = {100, 100, 75};
+            attackHitChance = accuracies;
+
+        }
+
+        if (pokemonName.equals("Pikachu")){
+
+            String[] pokemonMoves = {"Quick Attack", "Thunder Shock", "Slam", "Move4", "Move5", "Move6"};
+            possibleMoves = pokemonMoves;
+            int[] powers = {30, 50, 80};
+            attackPower = powers;
+            int[] accuracies = {100, 100, 85};
+            attackHitChance = accuracies;
+        }
+
+        if (pokemonName.equals("Charmander")){
+
+            String[] pokemonMoves = {"Ember", "Slash", "Flamethrower", "Move4", "Move5", "Move6"};
+            possibleMoves = pokemonMoves;
+            int[] powers = {40, 70, 90};
+            attackPower = powers;
+            int[] accuracies = {100, 85, 75};
+            attackHitChance = accuracies;
+        }
+
+        if (pokemonName.equals("Meowth")){
+
+            String[] pokemonMoves = {"Scratch", "Bite", "Slash", "Move4", "Move5", "Move6"};
+            possibleMoves = pokemonMoves;
+            int[] powers = {40, 60, 70};
+            attackPower = powers;
+            int[] accuracies = {100, 90, 85};
+            attackHitChance = accuracies;
+        }
+
+        if (pokemonName.equals("Houndour")){
+
+ 			String[] pokemonMoves = {"Bite", "Feint Attack", "Crunch", "Move4", "Move5", "Move6"}; 
+            possibleMoves = pokemonMoves;
+            int[] powers = {60, 70, 80};
+            attackPower = powers;
+            int[] accuracies = {100, 95, 90};
+            attackHitChance = accuracies;
+        }
+
+        if (pokemonName.equals("Koffing")){
+  
+            String[] pokemonMoves = {"Smog", "Tackle", "Sludge", "Move4", "Move5", "Move6"}; 
+            possibleMoves = pokemonMoves;
+            int[] powers = {30, 50, 70};
+            attackPower = powers;
+            int[] accuracies = {100, 90, 80};
+            attackHitChance = accuracies;
+        }
+    }
+}
